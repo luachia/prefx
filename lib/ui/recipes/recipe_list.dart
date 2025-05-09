a7515092c8bb19145a14f71e78e022dc0325af52
yellow buks <luachia3@gmail.com>
2025-05-09 16:50:10 +0545

    working Android version

--- lib/ui/recipes/recipe_list.dart
+++ lib/ui/recipes/recipe_list.dart
@@ -273,7 +273,7 @@ class _RecipeListState extends ConsumerState<RecipeList> {
                 child: Text(
                   snapshot.error.toString(),
                   textAlign: TextAlign.center,
-                  textScaler: TextScaler.linear(1.3),
+                  textScaler: const TextScaler.linear(1.3),
                 ),
               ),
             );
